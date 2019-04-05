2e903070b0256fb97d358a8fda9b19b5160d3e19
Nathan Simpson <ndsimpson01@gmail.com>
2019-04-05 16:46:05 -0600

    Completed problem 2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    family: ["lynne", "jim", "josh", "nate", "james"]
+  }
+
   render() {
+    let displayArray = this.state.family.map((person, index) => <h2 key={index}> {person} </h2>)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      {displayArray}
       </div>
     );
   }
